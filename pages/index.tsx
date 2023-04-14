7952cb1bbe2d9e972037fb9327f0b3ecc4f0a94b
KeshavaTelan <keshavasilva@gmail.com>
2023-04-14 12:55:20 +0530

    Created the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,32 @@
 // import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import '/styles/Home.module.css'
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
-import styles from '../styles/Home.module.css'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import "/styles/Home.module.css";
+import { MDBRow, MDBCol } from "mdb-react-ui-kit";
+import styles from "../styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-    <div className={styles.bg1}>
-               <MDBRow className={styles.bg1}>
-      <MDBCol xl='6' className='imge'>
-       {/* <Image src={'/side.svg'} alt='side' width={100} height={100}/>  */}
-       tests1
-      </MDBCol>
-      <MDBCol xl='6'>
-        test 2
-      </MDBCol>
-    </MDBRow>
-    </div>
+      <div >
+        <MDBRow>
+          <MDBCol xl="6" className="d-none d-xl-block d-xxl-block">
+            <img src="/side.svg" />
+          </MDBCol>
+          <MDBCol xl="6" className=" my-auto ">
+            <div className=" d-flex align-items-center justify-content-center">
+              <img src="/doctor.svg" />
+            </div>
+
+            <br />
+            <h2 className="text-center display-6 fw-bold text-primary">
+              Welcome to the Clinic Demo
+            </h2>
+          </MDBCol>
+        </MDBRow>
+      </div>
     </>
-  )
+  );
 }
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -3,14 +3,23 @@ import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
 export default function Signin() {
     return (
       <>
-          <MDBRow>
-      <MDBCol xl='6'>
-       4
-      </MDBCol>
-      <MDBCol xl='6'>
-        4
-      </MDBCol>
-    </MDBRow>
-      </>
+      <div >
+        <MDBRow>
+          <MDBCol xl="6" className="d-none d-xl-block d-xxl-block">
+            <img src="/side.svg" />
+          </MDBCol>
+          <MDBCol xl="6" className=" my-auto ">
+            <div className=" d-flex align-items-center justify-content-center">
+              <img src="/doctor.svg" />
+            </div>
+
+            <br />
+            <h2 className="text-center display-6 fw-bold text-primary">
+              Welcome to the Clinic Demo
+            </h2>
+          </MDBCol>
+        </MDBRow>
+      </div>
+    </>
     )
   }
