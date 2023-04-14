aaa4fd75bbee190a9858d36fe6606a4df969da2a
KeshavaTelan <keshavasilva@gmail.com>
2023-04-14 14:34:58 +0530

    created the login screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 // import Head from 'next/head'
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import "/styles/Home.module.css";
+//import "/styles/Home.module.css";
 import { MDBRow, MDBCol } from "mdb-react-ui-kit";
 import styles from "../styles/Home.module.css";
 
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,25 +1,103 @@
-import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+import {
+  MDBInput,
+  MDBCol,
+  MDBRow,
+  MDBCheckbox,
+  MDBBtn,
+  MDBContainer,
+  MDBIcon,
+} from "mdb-react-ui-kit";
+import styles from "../styles/signin.module.css";
 
 export default function Signin() {
-    return (
-      <>
-      <div >
+  return (
+    <>
+      <div>
         <MDBRow>
           <MDBCol xl="6" className="d-none d-xl-block d-xxl-block">
-            <img src="/side.svg" />
+            <img src="/doctorall.svg" />
           </MDBCol>
-          <MDBCol xl="6" className=" my-auto ">
-            <div className=" d-flex align-items-center justify-content-center">
-              <img src="/doctor.svg" />
-            </div>
 
+          <MDBCol xl="6" className=" my-auto ">
+            <MDBContainer className="w-50">
+              <div className="  ">
+                <h2 className=" display-6 fw-bold text-primary">
+                  Welcome Back !!
+                </h2>
+                <p className=" text-primary fw-bold ">
+                  To keep connected with us please <br />
+                  login with your personal info
+                </p>
+                <form className="mt-5">
+                  <MDBInput
+                    className="mb-4"
+                    type="email"
+                    id="form2Example1"
+                    label="Email address"
+                  />
+                  <MDBInput
+                    className="mb-4"
+                    type="password"
+                    id="form2Example2"
+                    label="Password"
+                  />
+
+                  <MDBRow className="mb-4">
+                    <MDBCol className="d-flex justify-content-center">
+                      <MDBCheckbox
+                        id="form2Example3"
+                        label="Remember me"
+                        defaultChecked
+                      />
+                    </MDBCol>
+                    <MDBCol>
+                      <a href="#!">Forgot password?</a>
+                    </MDBCol>
+                  </MDBRow>
+
+                  <MDBBtn
+                    type="submit"
+                    className="mb-4 border border-primary"
+                    block
+                    color="secondary"
+                  >
+                    Sign in
+                  </MDBBtn>
+
+                  <div className="text-center">
+                    <div className={styles.textcenter}>
+                      <div className={styles.texthr}>
+                        <span className={styles.texthrtext}>Or Login With</span>
+                      </div>
+                    </div>
+
+                    <div className="my-4">
+                      <MDBBtn floating color="secondary" className="mx-3">
+                        <MDBIcon fab icon="facebook-f" />
+                      </MDBBtn>
+
+                      <MDBBtn floating color="secondary" className="mx-3">
+                        <MDBIcon fab icon="google" />
+                      </MDBBtn>
+                    </div>
+                    <p className=" text-primary fw-bold ">
+                  Create an Account With Us
+                </p>
+                <MDBBtn
+                    type="submit"
+                    className="mb-4 border border-primary"
+                    block
+                    color='tertiary'
+                  >Get Started
+                  </MDBBtn>
+                  </div>
+                </form>
+              </div>
+            </MDBContainer>
             <br />
-            <h2 className="text-center display-6 fw-bold text-primary">
-              Welcome to the Clinic Demo
-            </h2>
           </MDBCol>
         </MDBRow>
       </div>
     </>
-    )
-  }
+  );
+}
