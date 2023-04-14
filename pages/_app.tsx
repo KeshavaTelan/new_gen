78c075ae845ef32cef07a063790921f85611b2b1
KeshavaTelan <keshavasilva@gmail.com>
2023-04-14 11:26:52 +0530

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '@/styles/globals.css'
+import 'mdb-react-ui-kit/dist/css/mdb.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import type { AppProps } from 'next/app'
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
---
+++ pages/signin.tsx
@@ -0,0 +1,16 @@
+import { MDBRow, MDBCol } from 'mdb-react-ui-kit';
+
+export default function Signin() {
+    return (
+      <>
+          <MDBRow>
+      <MDBCol xl='6'>
+       4
+      </MDBCol>
+      <MDBCol xl='6'>
+        4
+      </MDBCol>
+    </MDBRow>
+      </>
+    )
+  }
